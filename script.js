26db5ff699df379219c2e880223e7754fa96409d
dexter-999 <youssefzounid1@gmail.com>
2025-02-13 23:31:50 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+var $grid = $('.xgrid').isotope({});
+$('.filter-button-group').on('click' , 'button', function (){
+    var filterValue = $(this).attr('data-filter');
+    $grid.isotope({filter:filterValue});
+});
